94297ccd36e2123d109c2dd27fda36212b20ce22
Dexonose <38237153+Dexonose@users.noreply.github.com>
2024-08-05 11:08:34 +1000

    Delete Exercise1.py

--- Exercise1.py
+++
@@ -1,6 +0,0 @@
-#Prompt for input
-print("Input Width + Height to calculate area of rectangle")
-#Create a variable holding user input values
-vals = input("x y\n").strip().split()
-#Multiply values and output to console
-print("Area of rectangle: " + str(float(vals[0]) * float(vals[1])))
