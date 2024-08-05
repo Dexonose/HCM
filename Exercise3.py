a0e50ed0a547b178053d37a3b3b0813d1ee7ea56
Dexonose <38237153+Dexonose@users.noreply.github.com>
2024-08-05 11:08:18 +1000

    Deleted.py

--- Exercise3.py
+++
@@ -1,4 +0,0 @@
-#Split data into an array for easier referencing
-data = input("Input log file output:\n").split()
-#Output the data.
-print(f"On {data[0]} at {data[1]}, the IP address {data[2]} sent a request to the destination {data[3]}. A packet of the size {data[4]} was sent over the {data[5]} protocol.")
