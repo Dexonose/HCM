e823ea420b91095524d6a11202396a6d71b0d328
Dexonose <38237153+Dexonose@users.noreply.github.com>
2024-08-06 10:14:31 +1000

    Create Exercise_3.py

---
+++ Lesson 5/Exercise_3.py
@@ -0,0 +1,18 @@
+file = open("./Logs.txt")
+output_file = open("./Results.txt", "x")
+for line in file:
+    #Split data into an array for easier referencing
+    data = line.split()
+    #Check for suspicious activity
+    if data[2] == "192.168.1.2" or data[4] == "RDP" or int(data[5]) >= 1500:
+        #Output the data.
+        output = (f"Suspicious Activity On {data[0]} at {data[1]}, the IP address {data[2]} sent a request to the destination {data[3]}. A packet of the size {data[5]} was sent over the {data[4]} protocol.\n")
+        print(output)
+        output_file.write(output)
+    else:
+        output = ("No suspicious activity detected\n")
+        print(output)
+        output_file.write(output)
+file.close()
+output_file.close()
+print("Wrote to file 'Results.txt'")
